db8266147c75e082883be1c2ad78223f35803345
chirtu <chitufatuma45@gmail.com>
2025-03-13 19:14:31 +0300

    some changes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Footer from './Footer.jsx'
+
+import Header from './Header.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Footer />
+    
+    </>
+
+  )
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer (){
+    return (
+<footer>
+    <p>&copy; {new Date().getFullYear()} MarchWeb</p>
+</footer>
+    );
+}
+
+export default Footer;
